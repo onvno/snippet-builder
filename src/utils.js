fdf9405d2cfd8aaa66142efe338997689c07c565
onvno <leewei2020@126.com>
2018-08-19 11:13:57 +0800

    init

---
+++ src/utils.js
@@ -0,0 +1,39 @@
+/**
+ * 数组添加Commit
+ * Add Commit
+ * @param {Array} snippet 
+ */
+const handleCommit = ( snippet, name ) => {
+    const snName = name.slice(1);
+    
+    snippet.unshift(`/* start ${snName} */`);
+    snippet.push(`/* end ${snName} */`);
+
+    return snippet;
+}
+
+/**
+ * 文本转为数组
+ * Text Convert to Array
+ * @param {String} text 
+ */
+function buildBodyFromText(text) {
+    var fixed = text.replace(/\t/g, '\\t');
+    return fixed.split("\n");
+}
+
+/**
+ * 过滤隐藏文件或文件夹
+ * Filter hidden Filr or Folder
+ * https://stackoverflow.com/questions/18973655/how-to-ingnore-hidden-files-in-fs-readdir-result
+ * @param {Array} list 
+ */
+const filterMenu = (list) => {
+    return list.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
+}
+
+module.exports = {
+    handleCommit,
+    buildBodyFromText,
+    filterMenu,
+}
